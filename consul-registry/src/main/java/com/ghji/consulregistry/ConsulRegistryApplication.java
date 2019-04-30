7f18ff4d2af42c8d521ed93110a8d00f0462960a
jiaguohui <GYXTmZAvoI>
2019-04-30 19:27:38 +0800

    second commit

---
+++ consul-registry/src/main/java/com/ghji/consulregistry/ConsulRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.ghji.consulregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulRegistryApplication.class, args);
+	}
+
+}
